e885b6b73d5082466d3a91f5cc2026d49197e249
Jinso Raj <jinsoraj2000@gmail.com>
2023-10-20 19:34:58 +0530

    doctypes

---
+++ aikyam/aikyam/doctype/challenge/challenge.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, JinsoRaj and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.utils.nestedset import NestedSet
+
+class Challenge(NestedSet):
+	pass
