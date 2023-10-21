7239449795712c76f43a24f2f57c357c26432611
Jinso Raj <jinsoraj2000@gmail.com>
2023-10-21 13:12:55 +0530

    challenges platform

--- aikyam/aikyam/doctype/challenge/challenge.js
+++ aikyam/aikyam/doctype/challenge/challenge.js
@@ -1,8 +1,17 @@
 // Copyright (c) 2023, JinsoRaj and contributors
 // For license information, please see license.txt
+let create_challenge_url = frm => {
+	let challenge_name = frm.doc.challenge_name;
+	let challengeUrl = challenge_name.split(' ').join('_');
+	frm.set_value("challenge_url", challengeUrl);
+	frm.refresh_field("challenge_url");
+}
 
 frappe.ui.form.on('Challenge', {
 	// refresh: function(frm) {
+		challenge_name: (frm) =>{
+			create_challenge_url(frm);
+		}
 
 	// }
 });
---
+++ aikyam/aikyam/doctype/challenge_submission/test_challenge_submission.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, JinsoRaj and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestChallengeSubmission(FrappeTestCase):
+	pass
