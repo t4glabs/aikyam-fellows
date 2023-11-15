6435508c8ff4e047acb8bfc09effdbb7638a71e5
Aysha Hakeem <ayshapp468@gmail.com>
2023-11-15 15:49:20 +0530

    careers page

---
+++ aikyam/www/careers/index.py
@@ -0,0 +1,11 @@
+import frappe
+
+no_cache = 1
+no_sitemap = 1
+
+def get_context(context):
+    context.open_jobs = frappe.db.get_all( 'Job Opening', filters={'status': 'Open'}, fields =['job_title','route','custom_introduction']
+    )
+
+    context.show_sidebar = 1
+    return context
