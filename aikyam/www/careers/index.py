57cf39122931f617f6355c30a4e3b11235a9afe8
Aysha Hakeem <ayshapp468@gmail.com>
2023-11-16 09:50:15 +0530

    hide sidebar

--- aikyam/www/careers/index.py
+++ aikyam/www/careers/index.py
@@ -7,5 +7,5 @@ def get_context(context):
     context.open_jobs = frappe.db.get_all( 'Job Opening', filters={'status': 'Open'}, fields =['job_title','route','custom_introduction']
     )
 
-    context.show_sidebar = 1
+    # context.show_sidebar = 1
     return context
