b16c2b05f237f268151e25a09850d38c3875b568
Jinso Raj <jinsoraj2000@gmail.com>
2023-11-23 14:42:10 +0530

    compliance checklist

---
+++ aikyam/aikyam/doctype/compliance_checklist/compliance_checklist.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, JinsoRaj and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Compliance Checklist', {
+	// refresh: function(frm) {
+
+	// }
+});
