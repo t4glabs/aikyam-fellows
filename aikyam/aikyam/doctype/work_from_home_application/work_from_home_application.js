b48ca83212ebe971a4ac02ba7a463f1a2ca6517d
Jinso Raj <jinsoraj2000@gmail.com>
2023-11-23 06:50:50 +0000

    wfh doctype

---
+++ aikyam/aikyam/doctype/work_from_home_application/work_from_home_application.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, JinsoRaj and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Work From Home Application', {
+	// refresh: function(frm) {
+
+	// }
+});
